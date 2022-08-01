a41cf17aea3b9201a0cec686690eee91f8ef62a5
Mario Inga <mario21ic@gmail.com>
2022-07-31 21:53:10 -0500

    feat(global) improving pyusb example

--- pyusb/main.py
+++ pyusb/main.py
@@ -1,17 +1,25 @@
 import usb.core
 
-dev = usb.core.find(idVendor=0x0781, idProduct=0x5597)
+dev = usb.core.find(idVendor=0x0810, idProduct=0xe501)
+if not dev:
+    print("Could not find device :(")
+    exit(1)
+
 ep = dev[0].interfaces()[0].endpoints()[0]
-i = dev[0].interfaces()[0].bINterfaceNumber
+i = dev[0].interfaces()[0].bInterfaceNumber
+print("dev:", dev)
+print("ep:", ep)
 print("i:", i)
-exit()
 dev.reset()
 
+# To detach
 if dev.is_kernel_driver_active(i):
+    print("detach")
     dev.detach_kernel_driver(i)
 
 dev.set_configuration()
 eaddr = ep.bEndpointAddress
 
 r = dev.read(eaddr, 1024)
+print("r:", r)
 print(len(r))
